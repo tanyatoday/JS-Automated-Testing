edaa47d0991c51052c3fd6371d4a7d990e47e8cf
Татьяна <tbazhenova4@gmail.com>
2023-01-29 18:11:49 +0700

    update

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,10 @@
+import { strict as assert } from 'node:assert';
+
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('') === '');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('hello') === 'Hello');
 
 console.log('Все тесты пройдены!');
+
